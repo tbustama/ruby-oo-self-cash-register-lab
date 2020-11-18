0b0d0d54fff56832b3cf5eebb63d697ee8b7a026
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-18 15:39:15 -0800

    Cash lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,45 @@
+require 'pry'
+class CashRegister
+    attr_accessor :total, :discount
+    attr_reader :title_total, :price_total
+
+    def initialize(discount=nil)
+        @total = 0
+        @discount = discount
+        @title_total = []
+        @price_total = []
+    end
+
+    
+
+    def add_item(title, price, quantity=1)
+        @total += price * quantity
+        quantity.times {@title_total << title}
+        @price_total << price
+    end
+
+    def apply_discount
+        if @discount != nil
+            self.total = @total * 0.01 * (100 - @discount)
+            p "After the discount, the total comes to $#{@total.to_i}."
+        else 
+            p "There is no discount to apply."
+        end
+    end
+
+    def items
+        @title_total
+    end
+
+    def void_last_transaction
+        if @price_total.length == 1
+            self.total = 0.0
+        else
+            self.total = @total - @price_total[-1]
+        end
+    end
+end
+
+taylor = CashRegister.new(20)
+taylor.total = 1000
 
